14254d9b9c2b65244e2f97bd5d865346e84f2a9c
Ekwere Noble <ekwerenoble2@gmail.com>
2026-07-29 21:56:57 +0100

    __init__.py updated

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -1,10 +1,12 @@
 # server\app\__init__.py
 from flask import Flask
 from server.app.extensions import db
+from flask_migrate import Migrate
 
-# create the app
 app = Flask(__name__)
-# configure the SQLite database, relative to the app instance folder
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///store.db"
+
 # initialize the app with the extension
 db.init_app(app)
+
+migrate = Migrate(app, db)
