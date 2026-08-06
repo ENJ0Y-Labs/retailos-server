e6199657a6c01240ff80155877162f61bc083931
Ekwere Noble <ekwerenoble2@gmail.com>
2026-08-06 20:12:00 +0100

    server\app\utils\response.py updated

--- server/app/utils/response.py
+++ server/app/utils/response.py
@@ -1,17 +1,19 @@
 # server\app\utils\response.py
-def success_response(data=None, message=""):
-    return {
-        "success": True,
-        "data": data,
-        "message": message
-    }
 
-def error_response(code, message, fields=None):
-    return {
-        "success": False,
-        "error": {
-            "code": code,
-            "message": message,
-            "fields": fields or {}
+class Response:
+    def success_response(data=None, message=""):
+        return {
+            "success": True,
+            "data": data,
+            "message": message
         }
-    }
+
+    def error_response(code, message, fields=None):
+        return {
+            "success": False,
+            "error": {
+                "code": code,
+                "message": message,
+                "fields": fields or {}
+            }
+        }
