ffbd59dcf51a92a77fe14b2d38b52c5ee6097c0b
Ekwere Noble <ekwerenoble2@gmail.com>
2026-08-08 22:38:41 +0100

    __init__.py updated

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -2,11 +2,14 @@
 from flask import Flask
 from server.app.extensions import db
 from flask_migrate import Migrate
+from server.app.routes.auth_routes import auth_bp
 
 app = Flask(__name__)
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///store.db"
 
 # initialize the app with the extension
 db.init_app(app)
+
+app.register_blueprint(auth_bp, url_prefix="/auth")
 
 migrate = Migrate(app, db)
