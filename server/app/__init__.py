9352647a9386a7a5c5d6824a922fa65175bdda68
Ekwere Noble <ekwerenoble2@gmail.com>
2026-08-09 16:09:54 +0100

    secret key added

--- server/app/__init__.py
+++ server/app/__init__.py
@@ -9,7 +9,7 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///store.db"
 
 # initialize the app with the extension
 db.init_app(app)
-
+app.secret_key = '854c17e3094dc13670c236a3a3a7daf0397f89d43cf0d7942a1754ba1b3ae667'
 app.register_blueprint(auth_bp, url_prefix="/auth")
 
 migrate = Migrate(app, db)
