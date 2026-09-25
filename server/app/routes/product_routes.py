4348491243af0c3cad9acf4794ec2faab0646dbc
Ekwere Noble <ekwerenoble2@gmail.com>
2026-09-25 09:17:48 +0100

    Format route functions to match project style

--- server/app/routes/product_routes.py
+++ server/app/routes/product_routes.py
@@ -2,29 +2,41 @@ from flask import Blueprint
 from server.app.middleware.auth_middleware import require_session
 from server.app.services.product_service import ProductService
 
-product=ProductService()
-product_bp=Blueprint("product",__name__)
+product = ProductService()
+product_bp = Blueprint("product", __name__)
 
-@product_bp.route("/create",methods=["POST"])
-@require_session
-def create_product(): return product.create_product()
 
-@product_bp.route("/get",methods=["GET"])
+@product_bp.route("/create", methods=["POST"])
 @require_session
-def get_product(): return product.get_product()
+def create_product():
+    return product.create_product()
 
-@product_bp.route("/list",methods=["GET"])
-@require_session
-def list_products(): return product.list_products()
 
-@product_bp.route("/update",methods=["PATCH"])
+@product_bp.route("/get", methods=["GET"])
 @require_session
-def update_product(): return product.update_product()
+def get_product():
+    return product.get_product()
 
-@product_bp.route("/delete",methods=["DELETE"])
-@require_session
-def delete_product(): return product.delete_product()
 
-@product_bp.route("/adjust",methods=["POST"])
+@product_bp.route("/list", methods=["GET"])
 @require_session
-def adjust_stock(): return product.adjust_stock()
+def list_products():
+    return product.list_products()
+
+
+@product_bp.route("/update", methods=["PATCH"])
+@require_session
+def update_product():
+    return product.update_product()
+
+
+@product_bp.route("/delete", methods=["DELETE"])
+@require_session
+def delete_product():
+    return product.delete_product()
+
+
+@product_bp.route("/adjust", methods=["POST"])
+@require_session
+def adjust_stock():
+    return product.adjust_stock()
