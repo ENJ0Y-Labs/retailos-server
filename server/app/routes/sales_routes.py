b9ddd45b0f726fbf40a2dc0d074481f8bb0481e8
Ekwere Noble <ekwerenoble2@gmail.com>
2026-09-25 09:17:50 +0100

    Format route functions to match project style

--- server/app/routes/sales_routes.py
+++ server/app/routes/sales_routes.py
@@ -2,21 +2,29 @@ from flask import Blueprint
 from server.app.middleware.auth_middleware import require_session
 from server.app.services.sales_service import SalesService
 
-sales=SalesService()
-sales_bp=Blueprint("sales",__name__)
+sales = SalesService()
+sales_bp = Blueprint("sales", __name__)
 
-@sales_bp.route("",methods=["POST"])
-@require_session
-def create_sale():return sales.create_sale()
 
-@sales_bp.route("",methods=["GET"])
+@sales_bp.route("", methods=["POST"])
 @require_session
-def list_sales():return sales.list_sales()
+def create_sale():
+    return sales.create_sale()
 
-@sales_bp.route("/get",methods=["GET"])
-@require_session
-def get_sale():return sales.get_sale()
 
-@sales_bp.route("/receipt",methods=["GET"])
+@sales_bp.route("", methods=["GET"])
 @require_session
-def receipt():return sales.receipt()
+def list_sales():
+    return sales.list_sales()
+
+
+@sales_bp.route("/get", methods=["GET"])
+@require_session
+def get_sale():
+    return sales.get_sale()
+
+
+@sales_bp.route("/receipt", methods=["GET"])
+@require_session
+def receipt():
+    return sales.receipt()
