2036433c70e328d012c33065097ebf40e1cfdac1
Ekwere Noble <ekwerenoble2@gmail.com>
2026-09-25 09:58:46 +0100

    Clean test fixtures into project style

--- server/tests/conftest.py
+++ server/tests/conftest.py
@@ -1,22 +1,28 @@
-# server\tests\conftest.py
+# server/tests/conftest.py
 import pytest
+
 from server.app import create_app
 from server.app.config import TestConfig
 from server.app.extensions import db
 
+
 @pytest.fixture()
 def app():
     app = create_app(TestConfig)
-    
+
     with app.app_context():
         db.create_all()
+
         yield app
+
         db.session.remove()
         db.drop_all()
-    
+
+
 @pytest.fixture()
 def client(app):
     return app.test_client()
+
 
 @pytest.fixture()
 def runner(app):
