0e26d3a620b821fbd506ae07b157fe55e1853a68
Ekwere Noble <ekwerenoble2@gmail.com>
2026-09-25 11:07:33 +0100

    Add alert trigger and deletion support

--- server/app/services/alert_service.py
+++ server/app/services/alert_service.py
@@ -52,6 +52,7 @@ class AlertService:
 
         products = Product.query.filter(
             Product.store_id == store_id,
+            Product.low_stock_threshold.is_not(None),
             Product.stock_quantity <= Product.low_stock_threshold
         ).all()
 
@@ -115,4 +116,71 @@ class AlertService:
 
         return Response.success_response(
             message="ALERT_RESOLVED"
+        ), 200
+
+
+    def create_low_stock_alerts(self, store_id, product_ids=None):
+        query = Product.query.filter(
+            Product.store_id == store_id,
+            Product.low_stock_threshold.is_not(None),
+            Product.stock_quantity <= Product.low_stock_threshold
+        )
+
+        if product_ids:
+            query = query.filter(Product.id.in_(product_ids))
+
+        products = query.all()
+        created_alerts = []
+
+        for product in products:
+            existing_alert = Alert.query.filter_by(
+                store_id=store_id,
+                product_id=product.id,
+                type=AlertType.LOW_STOCK,
+                is_resolved=False
+            ).first()
+
+            if existing_alert:
+                continue
+
+            alert = Alert(
+                store_id=store_id,
+                product_id=product.id,
+                type=AlertType.LOW_STOCK,
+                message=f"{product.name} is low on stock"
+            )
+
+            db.session.add(alert)
+            created_alerts.append(product.id)
+
+        return created_alerts
+
+    def delete_alert(self):
+        store_id = request.args.get("store_id", type=int)
+        alert_id = request.args.get("id", type=int)
+
+        if not get_authorized_store(store_id):
+            return Response.error_response(
+                "STORE_ACCESS_DENIED",
+                "You do not have access to this store",
+                {}
+            ), 403
+
+        alert = Alert.query.filter_by(
+            id=alert_id,
+            store_id=store_id
+        ).first()
+
+        if not alert:
+            return Response.error_response(
+                "ALERT_NOT_FOUND",
+                "Alert not found",
+                {}
+            ), 404
+
+        db.session.delete(alert)
+        db.session.commit()
+
+        return Response.success_response(
+            message="ALERT_DELETED"
         ), 200
